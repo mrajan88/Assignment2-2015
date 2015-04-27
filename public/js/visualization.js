1d1a38a904df117109a6f9f5433cf28561f90ad2
Christian Memije <christianmemije@yahoo.com>
2015-04-26 20:10:26 -0700

    Added username

--- public/js/visualization.js
+++ public/js/visualization.js
@@ -23,7 +23,7 @@ var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
   .html(function(d) {
-    return "<strong>Frequency:</strong> <span style='color:red'>" + "</span>";
+    return "<strong>Frequency:</strong> <span style='color:red'>" + d.username + "</span>";
   })
 
 //create svg
